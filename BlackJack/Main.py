7306a4d471d0abee3e9162d6568de1c1f2305774
Helvis Tīcs <65508871+BeaT-7@users.noreply.github.com>
2020-07-07 21:52:16 +0300

    Add files via upload

--- BlackJack/Main.py
+++ BlackJack/Main.py
@@ -29,13 +29,13 @@ def Winnerr():
         if bot.Score() == 21:
             Winner = cfont.render(str(f'Draw'), 1, (255, 255, 255))
             screen.blit(Winner, (infoObject.current_w * 0.48, infoObject.current_h * 0.49))
-        elif bot.Score() <=15:
+        elif bot.Score() <= 15:
             while bot.Score() <= 15:
                 bot.GetCard(deck)
                 BotAceCheck()
-            if bot.Score() != 21:
-                Winner = cfont.render(str(f'{you.name} Wins'), 1, (255, 255, 255))
-                screen.blit(Winner, (infoObject.current_w * 0.48, infoObject.current_h * 0.49))
+        elif bot.Score() != 21:
+            Winner = cfont.render(str(f'{you.name} Wins'), 1, (255, 255, 255))
+            screen.blit(Winner, (infoObject.current_w * 0.48, infoObject.current_h * 0.49))
         EndRound()
     elif you.Score() > 21:
         Winner = cfont.render(str(f'{bot.name} Wins'), 1, (255, 255, 255))
